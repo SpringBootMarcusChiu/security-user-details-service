06eb7e851ead7f3904a4dc5fa880f77fda84bb1e
Marcus Chiu <marcuschiu9@gmail.com>
2020-12-28 13:03:33 -0600

    initial commit

---
+++ src/main/java/com/marcuschiu/securityuserdetailsservice/SecurityUserDetailsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.securityuserdetailsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityUserDetailsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityUserDetailsServiceApplication.class, args);
+	}
+
+}
